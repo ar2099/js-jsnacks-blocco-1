8ca82e0972d08cef1aef776159ea3d0aeb3ededc
Andrea <onemanshow2099@gmail.com>
2022-03-16 11:56:49 +0100

    primo esercizio completo

--- JSnack-1/script.js
+++ JSnack-1/script.js
@@ -6,4 +6,14 @@ numeroUno = Number(numeroUno);
 
 numeroDue = Number(numeroDue)
 
-if ()
+if (numeroUno < numeroDue) {
+   
+    document.getElementById("vFrase").innerHTML = `${numeroDue} - secondo numero`;
+
+} else if (numeroUno > numeroDue) {
+    document.getElementById("vFrase").innerHTML = `${numeroUno} - primo numero`;
+
+} else {
+    
+    document.getElementById("vFrase").innerHTML = "I due numeri sono uguali";
+}
